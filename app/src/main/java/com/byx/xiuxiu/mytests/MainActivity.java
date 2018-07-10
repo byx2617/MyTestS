564757d28b977a036e67812499738f71bb6bf759
John Doe <l>
2018-07-10 09:23:57 +0800

    测试提交

---
+++ app/src/main/java/com/byx/xiuxiu/mytests/MainActivity.java
@@ -0,0 +1,24 @@
+package com.byx.xiuxiu.mytests;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+
+
+    }
+
+    private void initView() {
+        tv = (TextView) findViewById(R.id.tv);
+        tv.setText("天下都是朕的！");
+    }
+}
